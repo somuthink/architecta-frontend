83eb3461b70eb7c8c8e338ef53692d997a8dc773
somuthink <qould12@gmail.com>
2025-01-27 01:27:32 +0300

    theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Switch } from "wouter";
 import "./index.css";
 import { generatePage } from "./pages/generate/generatePage";
+import { ThemeProvider } from "@/components/theme/themeProvider";
 
 function App() {
     return (
         <Switch>
-            <Route path="/" component={generatePage}></Route>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <div className="h-full">
+                    <Route path="/" component={generatePage}></Route>
+                </div>
+            </ThemeProvider>
         </Switch>
     );
 }
